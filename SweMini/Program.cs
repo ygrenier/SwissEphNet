acaae2818bd3c7152ff9eb7e8c33fca15ffc1a9e
Yanos <ygrenier@ygrenier.com>
2014-04-13 21:35:55 +0200

    Start SweMini implements

--- SweMini/Program.cs
+++ SweMini/Program.cs
@@ -89,75 +89,69 @@ using SweNet;
 namespace SweMini
 {
 
-    #region C
-
-//#include "swephexp.h" 	/* this includes  "sweodef.h" */
-
-//int main()
-//{
-//  char sdate[AS_MAXCH], snam[40], serr[AS_MAXCH];  
-//  int jday = 1, jmon = 1, jyear = 2000;
-//  double jut = 0.0;
-//  double tjd, te, x2[6];
-//  int32 iflag, iflgret;
-//  int p;
-//  iflag = SEFLG_SPEED;
-//  while (TRUE) {
-//    printf("\nDate (d.m.y) ?");
-//    /*gets(sdate);*/
-//    if( !fgets(sdate, sizeof(sdate)-1, stdin) ) return OK;
-//        /*
-//         * stop if a period . is entered
-//         */
-//    if (*sdate == '.') 
-//      return OK;
-//    if (sscanf (sdate, "%d%*c%d%*c%d", &jday,&jmon,&jyear) < 1) exit(1);
-//        /*
-//         * we have day, month and year and convert to Julian day number
-//         */
-//    tjd = swe_julday(jyear,jmon,jday,jut,SE_GREG_CAL);        
-//        /*
-//         * compute Ephemeris time from Universal time by adding delta_t
-//         */
-//    te = tjd + swe_deltat(tjd);
-//    printf("date: %02d.%02d.%d at 0:00 Universal time\n", jday, jmon, jyear);
-//    printf("planet     \tlongitude\tlatitude\tdistance\tspeed long.\n");
-//        /*
-//         * a loop over all planets
-//         */
-//    for (p = SE_SUN; p <= SE_CHIRON; p++) {
-//      if (p == SE_EARTH) continue;
-//        /*
-//         * do the coordinate calculation for this planet p
-//         */
-//      iflgret = swe_calc(te, p, iflag, x2, serr);
-//          /*
-//           * if there is a problem, a negative value is returned and an 
-//           * errpr message is in serr.
-//           */
-//      if (iflgret < 0) 
-//    printf("error: %s\n", serr);
-//      else if (iflgret != iflag)
-//    printf("warning: iflgret != iflag. %s\n", serr);
-//          /*
-//           * get the name of the planet p
-//           */
-//      swe_get_planet_name(p, snam);
-//          /*
-//           * print the coordinates
-//           */
-//      printf("%10s\t%11.7f\t%10.7f\t%10.7f\t%10.7f\n",
-//         snam, x2[0], x2[1], x2[2], x2[3]);
-//    }
-//  }
-//  return OK;
-//}
-
-    #endregion
-
     class Program
     {
         static int Main(string[] args) {
+            string sdate = String.Empty, snam = String.Empty, serr = String.Empty;
+            int jday = 1, jmon = 1, jyear = 2000;
+            double jut = 0.0;
+            double tjd, te;
+            double[] x2 = new double[6];
+            Int32 iflag, iflgret;
+            int p;
+            iflag = SwissEph.SEFLG_SPEED;
+            using (var swe = new SwissEph()) {
+                while (true) {
+                    printf("\nDate (d.m.y) ?");
+                    /*gets(sdate);*/
+                    //    if( !fgets(sdate, sizeof(sdate)-1, stdin) ) return OK;
+                    sdate = Console.ReadLine();
+                    if (String.IsNullOrWhiteSpace(sdate)) return SwissEph.OK;
+                    /*
+                     * stop if a period . is entered
+                     */
+                    if (sdate == ".")
+                        return SwissEph.OK;
+                    if (C.sscanf(sdate, "%d%*c%d%*c%d", ref jday, ref jmon, ref jyear) < 1) return 1;
+                    /*
+                     * we have day, month and year and convert to Julian day number
+                     */
+                    tjd = swe.JulDay(jyear, jmon, jday, jut, SwissEph.SE_GREG_CAL);        
+                    /*
+                     * compute Ephemeris time from Universal time by adding delta_t
+                     */
+                    te = tjd + swe.DeltaT(tjd);
+                    printf("date: %02d.%02d.%d at 0:00 Universal time\n", jday, jmon, jyear);
+                    printf("planet     \tlongitude\tlatitude\tdistance\tspeed long.\n");
+                    /*
+                     * a loop over all planets
+                     */
+                    for (p = SwissEph.SE_SUN; p <= SwissEph.SE_CHIRON; p++) {
+                        if (p == SwissEph.SE_EARTH) continue;
+                        /*
+                         * do the coordinate calculation for this planet p
+                         */
+                        //      iflgret = swe_calc(te, p, iflag, x2, serr);
+                        /*
+                         * if there is a problem, a negative value is returned and an 
+                         * errpr message is in serr.
+                         */
+                        //      if (iflgret < 0) 
+                        //    printf("error: %s\n", serr);
+                        //      else if (iflgret != iflag)
+                        //    printf("warning: iflgret != iflag. %s\n", serr);
+                        /*
+                         * get the name of the planet p
+                         */
+                        snam = swe.GetPlanetName(p);
+                        /*
+                         * print the coordinates
+                         */
+                        printf("%10s\t%11.7f\t%10.7f\t%10.7f\t%10.7f\n",
+                           snam, x2[0], x2[1], x2[2], x2[3]);
+                    }
+                }
+            }
 
 #if DEBUG
             Console.Write("\nPress a key to quit...");
