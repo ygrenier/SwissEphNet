eed03527333fa76e44ffee83231034e1f50c027c
Yanos <ygrenier@ygrenier.com>
2014-04-13 09:54:46 +0200

    SwissEph is disposable

--- SweNet/SwissEph.cs
+++ SweNet/SwissEph.cs
@@ -8,7 +8,31 @@ namespace SweNet
     /// <summary>
     /// Swiss Ephemeris context
     /// </summary>
-    public partial class SwissEph
+    public partial class SwissEph : IDisposable
     {
+        #region Ctors & Dest
+
+        /// <summary>
+        /// Create a new context
+        /// </summary>
+        public SwissEph() {
+        }
+
+        /// <summary>
+        /// Internal release resources
+        /// </summary>
+        protected virtual void Dispose(bool disposing) {
+        }
+
+        /// <summary>
+        /// Release resources
+        /// </summary>
+        public void Dispose() {
+            Dispose(true);
+        }
+
+        #endregion
+
     }
+
 }
