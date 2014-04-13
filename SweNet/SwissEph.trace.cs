949384f1e18b1f18329bcade0059f369ab943813
Yanos <ygrenier@ygrenier.com>
2014-04-13 19:49:36 +0200

    -

---
+++ SweNet/SwissEph.trace.cs
@@ -0,0 +1,36 @@
+﻿/// <summary>
+/// trace management
+/// </summary>
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+
+namespace SweNet
+{
+    partial class SwissEph
+    {
+
+#if TRACE
+        /// <summary>
+        /// Trace a message or C code
+        /// </summary>
+        /// <param name="isC"></param>
+        /// <param name="format"></param>
+        /// <param name="args"></param>
+        protected void trace(bool isC, String format, params object[] args) {
+            var h = OnTrace;
+            if (h != null)
+                h(this, new TraceEventArgs(C.sprintf(format, args), isC));
+        }
+
+        /// <summary>
+        /// Event raised when a trace is emit
+        /// </summary>
+        public event EventHandler<TraceEventArgs> OnTrace;
+#endif
+
+    }
+}
---
+++ SweNet/[events].cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SweNet
+{
+
+    /// <summary>
+    /// Arguments for trace event
+    /// </summary>
+    public class TraceEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Create new arguments
+        /// </summary>
+        public TraceEventArgs(String message, bool isCTrace = false) {
+            this.Message = message;
+            this.IsCTrace = isCTrace;
+        }
+
+        /// <summary>
+        /// Message
+        /// </summary>
+        public String Message { get; private set; }
+
+        /// <summary>
+        /// Is a C trace ?
+        /// </summary>
+        public bool IsCTrace { get; private set; }
+    }
+
+    /// <summary>
+    /// Arguments for print event
+    /// </summary>
+    public class PrintEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Create new arguments
+        /// </summary>
+        public PrintEventArgs(String line) {
+            this.Line = line;
+        }
+
+        /// <summary>
+        /// Line printed
+        /// </summary>
+        public String Line { get; private set; }
+    }
+
+}
