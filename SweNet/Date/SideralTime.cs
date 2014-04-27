2378f3362b0ab1969e2b7d81da2cac3d29929e62
Yanos <ygrenier@ygrenier.com>
2014-04-27 18:53:41 +0200

    Create a struct for sideral time value

---
+++ SweNet/Date/SideralTime.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SweNet
+{
+
+    /// <summary>
+    /// Sideral time value
+    /// </summary>
+    public struct SideralTime
+    {
+        /// <summary>
+        /// Create a new sideral time
+        /// </summary>
+        public SideralTime(Double time)
+            : this() {
+            this.Value = time;
+        }
+
+        /// <summary>
+        /// Convert to string
+        /// </summary>
+        public override string ToString() {
+            return SweFormat.FormatAsTime(Value);
+        }
+
+        /// <summary>
+        /// Value of the sideral time
+        /// </summary>
+        public Double Value { get; private set; }
+    }
+
+}
