6d7b98d11344d511535114edcbf06533d99e5606
Yanos <ygrenier@ygrenier.com>
2014-05-01 20:08:24 +0200

    ArrayExtensions test cases.

---
+++ Tests/SwissEphNet.Tests/ArrayExtensionsTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SwissEphNet.Tests
+{
+    [TestClass]
+    public class ArrayExtensionsTest
+    {
+        [TestMethod]
+        public void TestGetPointer() {
+            var array = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            var target = array.GetPointer();
+            Assert.AreEqual(10, target.Length);
+            Assert.AreEqual(0, target[0]);
+
+            target = array.GetPointer(5);
+            Assert.AreEqual(5, target.Length);
+            Assert.AreEqual(5, target[0]);
+
+        }
+    }
+}
