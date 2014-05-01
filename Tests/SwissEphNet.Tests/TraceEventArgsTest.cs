7e1a551f8b21e48b9640e02592a59d258913cb23
Yanos <ygrenier@ygrenier.com>
2014-05-01 20:12:37 +0200

    TraceEventArgs test cases.

---
+++ Tests/SwissEphNet.Tests/TraceEventArgsTest.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SwissEphNet.Tests
+{
+    [TestClass]
+    public class TraceEventArgsTest
+    {
+        [TestMethod]
+        public void TestCreate() {
+            var target = new TraceEventArgs("message");
+            Assert.AreEqual("message", target.Message);
+        }
+    }
+}
