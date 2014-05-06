5b49f2686617c80048cc9eb86990a4cdd34378cc
Yanos <ygrenier@ygrenier.com>
2014-05-06 21:00:43 +0200

    Display error for each planet calculation.

---
+++ SweWPF/Converters/StringEmptyToVisibility.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SweWPF.Converters
+{
+
+    public class StringEmptyToVisibility : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            bool isVisible = !(value == null || String.IsNullOrWhiteSpace(value.ToString()));
+            if (parameter != null && parameter.ToString() == "not")
+                isVisible = !isVisible;
+            return isVisible ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture) {
+            throw new NotImplementedException();
+        }
+    }
+
+}
