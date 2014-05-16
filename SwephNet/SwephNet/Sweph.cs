ff482655221c1012863f45f530b356d0ff8c9c8e
Yanos <ygrenier@ygrenier.com>
2014-05-16 20:41:34 +0200

    Create a new project (and his test project) for the full .Net implementation

---
+++ SwephNet/SwephNet/Sweph.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwephNet
+{
+
+    /// <summary>
+    /// Swiss Ephmeris engine
+    /// </summary>
+    public class Sweph : IDisposable
+    {
+
+        #region Ctors & Dest
+
+        /// <summary>
+        /// Create a new engine
+        /// </summary>
+        public Sweph() {
+        }
+
+        /// <summary>
+        /// Internal release resources
+        /// </summary>
+        protected virtual void Dispose(bool disposing) {
+            if (disposing) {
+                // TODO Dispose resources
+            }
+        }
+
+        /// <summary>
+        /// Release resources
+        /// </summary>
+        public void Dispose() {
+            Dispose(true);
+        }
+
+        #endregion
+
+    }
+
+}
---
+++ Tests/SwephNet.Tests/SwephNetTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SwephNet.Tests
+{
+    [TestClass]
+    public class SwephNetTest
+    {
+
+        [TestMethod]
+        public void TestCreate() {
+            using (var swe = new Sweph()) {
+                //
+            }
+        }
+
+    }
+}
