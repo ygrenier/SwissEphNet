25ce43446738b21541d8135d6abee393f9f919b7
Yanos <ygrenier@ygrenier.com>
2014-05-16 20:41:34 +0200

    Create a new project (and his test project) for the full .Net implementation

---
+++ SwephNet/SwephNet/Properties/AssemblyInfo.cs
@@ -0,0 +1,30 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Les informations générales relatives à un assembly dépendent de 
+// l'ensemble d'attributs suivant. Changez les valeurs de ces attributs pour modifier les informations
+// associées à un assembly.
+[assembly: AssemblyTitle("SwephNet")]
+[assembly: AssemblyDescription("Swiss Ephemeris for .Net")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Yan Grenier")]
+[assembly: AssemblyProduct("SwephNet")]
+[assembly: AssemblyCopyright("Copyright © Yan Grenier 2014")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Les informations de version pour un assembly se composent des quatre valeurs suivantes :
+//
+//      Version principale
+//      Version secondaire 
+//      Numéro de build
+//      Révision
+//
+// Vous pouvez spécifier toutes les valeurs ou utiliser par défaut les numéros de build et de version 
+// en utilisant '*', comme indiqué ci-dessous :
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
