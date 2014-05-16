4faf1456f31f97559b3b5e2b7c7f10f278c8a8f6
Yanos <ygrenier@ygrenier.com>
2014-05-16 23:00:07 +0200

    Start simple implementation

---
+++ SwephNet/SwephNet/Dependency/IDependencyContainer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwephNet.Dependency
+{
+
+    /// <summary>
+    /// Interface for dependency container
+    /// </summary>
+    public interface IDependencyContainer : IDisposable
+    {
+
+
+
+    }
+
+}
---
+++ SwephNet/SwephNet/Dependency/IDependencyResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwephNet
+{
+
+    /// <summary>
+    /// Interface for dependency resolver
+    /// </summary>
+    public interface IDependencyResolver
+    {
+
+        /// <summary>
+        /// Indicate if we can resolve a type
+        /// </summary>
+        bool CanResolve<T>();
+
+        /// <summary>
+        /// Resolve a type
+        /// </summary>
+        T Resolve<T>();
+
+        /// <summary>
+        /// Create a new instance of a type
+        /// </summary>
+        T Create<T>();
+
+    }
+
+}
