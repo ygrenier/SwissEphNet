24320a371a791f757c8fd8e4fab478adb5591ab7
Yanos <ygrenier@ygrenier.com>
2014-05-16 23:00:07 +0200

    Start simple implementation

---
+++ SwephNet/SwephNet/Dependency/SimpleContainer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SwephNet.Dependency
+{
+
+    /// <summary>
+    /// Simple dependency container
+    /// </summary>
+    class SimpleContainer //: IDependencyResolver, IDependencyContainer
+    {
+
+    }
+
+}
