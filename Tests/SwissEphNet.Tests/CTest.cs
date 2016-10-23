d14b361fd6063762f57bfe38961d83e9917bcaa3
Yan Grenier <ygrenier@ygrenier.com>
2016-10-23 11:22:05 +0200

    Prepare a xunit tests project.

---
+++ Tests/SwissEphNet.Tests/CTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Xunit;
+
+namespace SwissEphNet.Tests
+{
+
+    public class CTest
+    {
+    
+        [Fact]
+        public void TestAtof() {
+            Assert.Equal(0.0, C.atof(null));
+            Assert.Equal(0.0, C.atof(""));
+            Assert.Equal(0.0, C.atof("test"));
+            Assert.Equal(0.0, C.atof("0"));
+            Assert.Equal(0.0, C.atof("0.0"));
+            Assert.Equal(1.0, C.atof("1"));
+            Assert.Equal(1.2, C.atof("1.2"));
+            Assert.Equal(1.0, C.atof("+1"));
+            Assert.Equal(1.2, C.atof("+1.2"));
+            Assert.Equal(-1.0, C.atof("-1"));
+            Assert.Equal(-1.2, C.atof("-1.2"));
+        }
+
+        [Fact]
+        public void TestFmod()
+        {
+            Assert.Equal(1.0, C.fmod(3, 2), 8);
+            Assert.Equal(1.3, C.fmod(5.3, 2), 8);
+            Assert.Equal(1.7, C.fmod(18.5, 4.2), 8);
+            Assert.Equal(0.5, C.fmod(18.5, 1), 8);
+            Assert.Equal(0.5, C.fmod(5.7, 1.3), 8);
+        }
+
+    }
+}
---
+++ Tests/SwissEphNet.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SwissEphNet.Tests")]
+[assembly: AssemblyTrademark("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7d8fdc4d-94fb-4fb7-9457-23ddde1de55a")]
