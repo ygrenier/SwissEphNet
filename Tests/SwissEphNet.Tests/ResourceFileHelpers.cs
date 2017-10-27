468e9abae989592e5ceffd031e2f208c391e33a6
Yanos <ygrenier@ygrenier.com>
2017-10-27 19:50:25 +0200

    Create a test to validate the issue.

---
+++ Tests/SwissEphNet.Tests/ResourceFileHelpers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SwissEphNet.Tests
+{
+    public static class ResourceFileHelpers
+    {
+        public static Stream OpenResourceFile(string name)
+        {
+            var asm = typeof(ResourceFileHelpers).GetAssembly();
+            String sr = $"SwissEphNet.Tests.files.{name}".Replace("/", ".").Replace("\\", ".");
+            return asm.GetManifestResourceStream(sr);
+        }
+    }
+}
