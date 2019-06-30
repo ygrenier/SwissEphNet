c2708b859a94a2e163990f1e3ba859f79a78b011
Yanos <ygrenier@ygrenier.com>
2019-06-30 09:39:27 +0200

    Fix test version.

--- Tests/SwissEphNet.Tests/SwissEphTest.cs
+++ Tests/SwissEphNet.Tests/SwissEphTest.cs
@@ -43,7 +43,7 @@ namespace SwissEphNet.Tests
         public void TestVersion() {
             using (var target = new SwissEph()) {
                 Assert.Equal("2.07.01", target.swe_version());
-                Assert.Equal("2.07.01-net-0002", target.swe_dotnet_version());
+                Assert.Equal("2.07.01-net-0003", target.swe_dotnet_version());
             }
         }
 
